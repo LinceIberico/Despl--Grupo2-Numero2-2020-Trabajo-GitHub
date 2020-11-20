f8975bf21764dde9c96a6b76be299ef479526cef
carlosmunioz99 <61143085+carlosmunioz99@users.noreply.github.com>
2020-11-20 10:48:03 +0100

    Creacion carpeta JS

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let oFlecha=document.getElementById("flecha");
+//Posicionando la flecha
+oFlecha.style.left=(window.innerWidth-2*oFlecha.offsetWidth)+"px";
+oFlecha.style.top=(window.innerHeight-70)+"px";
+
+
+let marcador=null;
+
+function subir(){
+  	window.scrollBy(0,-10);
+      clearInterval(marcador);
+    if(window.scrollY>1)
+    hacerScroll();
+}
+
+function hacerScroll() {
+    clearInterval(marcador);
+    marcador=setInterval(subir,0);    
+}
